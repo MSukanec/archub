e3441fb140015829d9bb4e1aafb83b2022241d69
matusukanec <39943890-matusukanec@users.noreply.replit.com>
2025-07-26 23:38:45 +0000

    Restored to '6eac2ab937a484d00792c4e4e715c900bdf2e580'
    Replit-Restored-To: 6eac2ab937a484d00792c4e4e715c900bdf2e580

--- src/components/modal/modals/admin/TaskGroupCreatorModal.tsx
+++ src/components/modal/modals/admin/TaskGroupCreatorModal.tsx
@@ -261,7 +261,7 @@ export function TaskGroupCreatorModal({ modalData, onClose }: TaskGroupCreatorMo
           name_template: `${data.name}.`,
           task_group_id: newGroup.id,
           unit_id: data.unit_id,
-          task_code: null,
+          task_code: 'AUTO',
         })
 
         // Update the task group with the template_id
